6132c804a4fe79786e4c504d2fbb72f7861d1335
Герман Труханов <germantruhanov@gmail.com>
2019-10-06 16:48:08 +0400

    06/10/19

--- DataPharser.cs
+++ DataPharser.cs
@@ -8,6 +8,13 @@ namespace Encryption_Task8
 {
     internal class DataPharser
     {
+        private char[] getPart(char[] mass, int size)
+        {
+            char[] result = new char[size];
+            Array.Copy(mass, mass.Length - size, result, 0, size);
+            return result;
+        }
+
         public List<char> getKey()
         {
             using (StreamReader stream = new StreamReader("Key.txt", Encoding.Default))
@@ -21,7 +28,7 @@ namespace Encryption_Task8
             using (StreamReader stream = new StreamReader(filePath, Encoding.Default))
             {
                 var tmp = stream.ReadToEnd();
-                char[] eng = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z' };
+                char[] eng = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z', 'W' };
                 List<string> message = tmp.Split().ToList();
                 string result = "";
                 foreach (var item in message)
@@ -40,9 +47,24 @@ namespace Encryption_Task8
             var result = new Dictionary<char, char>();
             var key = getKey().Distinct().ToList();
             var alphabet = GetAlphabet().Except(key).ToList();
-            for (int i = 0; i < key.Count; i++)
+            int keySize = key.Count;
+            int alphabetSize = alphabet.Count;
+            int size;
+            if (alphabetSize > keySize)
+            {
+                size = keySize;
+            }
+            else
+            {
+                size = alphabetSize;
+            }
+            for (int i = 0; i < size; i++)
             {
                 result.Add(key[i], alphabet[i]);
+            }
+            foreach (var item in getLostPairs(key, alphabet))
+            {
+                result.Add(item.Key, item.Value);
             }
             return result;
         }
@@ -77,6 +99,80 @@ namespace Encryption_Task8
                 }
             }
             return result;
+        }
+        private Dictionary<char, char> getLostPairs(List<char> firstList, List<char> secondList)
+        {
+            var dic = new Dictionary<char, char>();
+            int firstListSize = firstList.Count;
+            int secondListSize = secondList.Count;
+            int difference = 0;
+            bool isOdd = false;
+            List<char> tmpList;
+            if (firstListSize > secondListSize)
+            {
+                difference = firstListSize - secondListSize;
+                if (difference % 2 != 0)
+                {
+                    isOdd = true;
+                }
+                tmpList = getPart(firstList.ToArray(), difference).ToList();
+                if (tmpList.Count == 1)
+                {
+                    dic.Add(tmpList[0], '@');
+                }
+                else if (tmpList.Count == 0)
+                {
+                    Console.WriteLine("Error");
+                }
+                else if (isOdd)
+                {
+                    for (int i = 0; i < tmpList.Count - 2; i++)
+                    {
+                        dic.Add(tmpList[i], tmpList[i + 1]);
+                    }
+                    dic.Add(tmpList[tmpList.Count - 1], '#');
+                }
+                else
+                {
+                    for (int i = 0; i < tmpList.Count - 1; i++)
+                    {
+                        dic.Add(tmpList[i], tmpList[i + 1]);
+                    }
+                }
+            }
+            else if (secondListSize > firstListSize)
+            {
+                difference = secondListSize - firstListSize;
+                if (difference % 2 != 0)
+                {
+                    isOdd = true;
+                }
+                tmpList = getPart(secondList.ToArray(), difference).ToList();
+                if (tmpList.Count == 1)
+                {
+                    dic.Add(tmpList[0], '@');
+                }
+                else if (tmpList.Count == 0)
+                {
+                    Console.WriteLine("Error");
+                }
+                else if (isOdd)
+                {
+                    for (int i = 0; i < tmpList.Count - 2; i++)
+                    {
+                        dic.Add(tmpList[i], tmpList[i + 1]);
+                    }
+                    dic.Add(tmpList[tmpList.Count - 1], '#');
+                }
+                else
+                {
+                    for (int i = 0; i < tmpList.Count - 1; i++)
+                    {
+                        dic.Add(tmpList[i], tmpList[i + 1]);
+                    }
+                }
+            }
+            return dic;
         }
 
         public void watchPairs()
